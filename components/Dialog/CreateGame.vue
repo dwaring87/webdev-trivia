530e86735c80775e5229514c71a049ce8f83e88d
David Waring <dev@davidwaring.net>
2023-12-14 19:21:07 -0500

    Create Game: fix date timezone issue

--- components/Dialog/CreateGame.vue
+++ components/Dialog/CreateGame.vue
@@ -37,7 +37,11 @@
   watch(() => props.open, (v) => {
     if ( v ) {
       nextTick(() => {
-        date.value = new Date().toISOString().split('T')[0];
+        const now = new Date();
+        const y = now.getFullYear();
+        const m = String(now.getMonth() + 1).padStart(2, '0');
+        const d = String(now.getDate()).padStart(2, '0');
+        date.value = `${y}-${m}-${d}`;
         host.value = displayName.value;
         owner.value = displayName.value ? `${displayName.value} <${email.value}>` : email.value;
         host_ref.value.focus();
