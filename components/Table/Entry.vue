2ec3407c55e24a7338a7903805221d17ebd6e8c3
David Waring <dev@davidwaring.net>
2023-12-17 13:30:56 -0500

    Entry: disable delete team when not editable

--- components/Table/Entry.vue
+++ components/Table/Entry.vue
@@ -5,14 +5,14 @@
     team: String
   });
 
-  const { teamScores } = useDatabase();
+  const { editable, teamScores } = useDatabase();
   const showDeleteDialog = ref(false);
 </script>
 
 <template>
-  <td class="group cursor-pointer">
-    <p class="group-hover:hidden">{{ teamScores(props.team).entry }}</p>
-    <p class="hidden group-hover:block" @click="showDeleteDialog = true">
+  <td :class="[editable ? 'cursor-pointer' : 'cursor-default', 'group']">
+    <p :class="[editable ? 'block group-hover:hidden' : 'block']">{{ teamScores(props.team).entry }}</p>
+    <p :class="[editable ? 'hidden group-hover:block' : 'hidden']" @click="showDeleteDialog = editable ? true : false">
       <MdiDelete class="inline text-rose-800" />
     </p>
     <DialogDeleteTeam :open="showDeleteDialog" :team="team" @close="showDeleteDialog = false" />
